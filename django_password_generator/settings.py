46aacf259f173b0b0f9600e78f9123d635128628
francosparn <francosparn@gmail.com>
2022-12-28 17:34:13 -0300

    add module

--- django_password_generator/settings.py
+++ django_password_generator/settings.py
@@ -55,7 +55,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'django_password_generator.urls'
