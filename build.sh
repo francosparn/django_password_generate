d2de1bfe5f1e61ac45914c2dc4a24b2a8fa6d3a8
francosparn <francosparn@gmail.com>
2022-12-28 17:11:48 -0300

    commented pip install

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-# exit on error
+# exit on errorgit
 set -o errexit
 
 # poetry install
-pip install -r requirements.txt
+# pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
